b408723dcc19f29c830929c1c07675cd58aae990
Alejandro Otero Ortiz de Cosca <otero.alx@gmail.com>
2016-11-03 20:41:45 +0000

    TG-24 TG-25 Added Track model for group theme-related Activities.

--- apps/congress/models.py
+++ apps/congress/models.py
@@ -12,6 +12,9 @@ class Edition(BaseModel):
         default=datetime.date.today().year,
         unique=True
     )
+class Track(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = HTMLField(blank=True)
 
     def __str__(self):
         return self.name

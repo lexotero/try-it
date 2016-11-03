6aa563e04592f04de90a21671049770ad8546b4c
Alejandro Otero Ortiz de Cosca <otero.alx@gmail.com>
2016-11-03 01:40:39 +0000

    TG-5  #closed Now the "name" of the "Company" is unique.

--- apps/congress/models.py
+++ apps/congress/models.py
@@ -18,7 +18,7 @@ class Edition(BaseModel):
 
 
 class Company(BaseModel):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     description = HTMLField(blank=True)
 

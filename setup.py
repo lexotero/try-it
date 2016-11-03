d8eb64fa8bb747f9e270f3ed1e213f8f4d0a864e
Alejandro Otero Ortiz de Cosca <otero.alx@gmail.com>
2016-11-03 01:12:08 +0000

    Updated requirements.

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     author_email='otero.alx@gmail.com',
     license='Apache License 2.0',
     install_requires=[
-        'django',
-        'django-jet',
-        'django-tinymce'
+        'django>=1.10.3',
+        'django-jet==1.0.2',
+        'django-tinymce==2.4.0'
     ],
     extras_require={
         'development': [
